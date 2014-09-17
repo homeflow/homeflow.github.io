cd2ce20c17946eac14955c1c69609212338e1f63
Daniel Miller <millerpils@gmail.com>
2014-09-17 13:58:03 +0100

    Mobile menu up top

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 ( function( $ ) {
 $( document ).ready(function() {
-$('#cssmenu > ul > li > a').click(function() {
-  $('#cssmenu li').removeClass('active');
+$('.helper-menu > ul > li > a').click(function() {
+  $('.helper-menu li').removeClass('active');
   $(this).closest('li').addClass('active');	
   var checkElement = $(this).next();
   if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
@@ -9,7 +9,7 @@ $('#cssmenu > ul > li > a').click(function() {
     checkElement.slideUp('normal');
   }
   if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-    $('#cssmenu ul ul:visible').slideUp('normal');
+    $('.helper-menu ul ul:visible').slideUp('normal');
     checkElement.slideDown('normal');
   }
   if($(this).closest('li').find('ul').children().length == 0) {
